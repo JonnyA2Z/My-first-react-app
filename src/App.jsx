f1b4f6e956ed3dacb8df4c8e051979038092d548
JonnyA2Z <alvarez.jhonjhon17@gmail.com>
2025-03-28 14:15:46 +0800

    gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,30 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Search from './components/Search.jsx';
 
-const Card = ({title}) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect( () => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  });
-
-
-  return (
-    <div className='card' onClick={() => setCount(count + 1)} >
-      <h2>{title} <br/> {count}</h2>
-
-      <button className='button' onClick={() => setHasLiked(!hasLiked)}>
-       {hasLiked ? '❤️' : '🤍'}
-    
-        </button>
-    </div>
-  )
-}
 const App = () => {
- 
-
+  const [searchTerm, setSearchTerm] = useState('');
 
   return (
-    <div className='card-container'>
-    <Card  title="Star Wars" rating={5} isCool={true} actors={[{ name: 'Actors'}]} />
-    <Card  title="Avatar" />
-    <Card  title="The Lion King"/>
+   <main className="min-h-screen bg-cover bg-center bg-no-repeat"
+   style={{ backgroundImage: "url('/hero-bg.png')" }}>
+
+  <div className="pattern"/>  
+
+
+  <div className="wrapper">  
+    <header>
+      <img src='./hero.png' alt='Hero Banner'/>
+    <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+    </header> 
+
+    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
     </div>
+   </main>
+
+  
+  
   )
+  
 }
 
-export default App
+export default App;
